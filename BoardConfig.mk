LineageOS/android_device_huawei_angler

Add device compatibility matrix
Test: m system_manifest.xml -j (checks compatibility against
    framewrok manifest)
Bug: 37954458
Change-Id: I3d3531035cefcc60b5d9093666c032f3cf8e5687

BoardConfig.mk
@@ -142,5 +142,6 @@ USE_CLANG_PLATFORM_BUILD := true
 TARGET_FS_CONFIG_GEN += device/huawei/angler/config.fs
 
 DEVICE_MANIFEST_FILE := device/huawei/angler/manifest.xml
+DEVICE_MATRIX_FILE := device/huawei/angler/compatibility_matrix.xml
 
 -include vendor/huawei/angler/BoardConfigVendor.mk

dmoe1975/android_device_moto_shamu-dec152017

BoardConfig.mk
@@ -134,6 +134,7 @@ DONT_DEXPREOPT_PREBUILTS := true
 TARGET_FS_CONFIG_GEN += device/moto/shamu/config.fs
 
 DEVICE_MANIFEST_FILE := device/moto/shamu/manifest.xml
+DEVICE_MATRIX_FILE := device/moto/shamu/compatibility_matrix.xml
 
 -include vendor/moto/shamu/BoardConfigVendor.mk
 